aa8e57add483b59818ed14d0f5a7e5b22d14c785
afcarioca <afcarioca@hotmail.com>
2021-01-29 21:58:38 +0000

    first commit

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Head from 'next/head';
+
+
+  const BackgroundImage = styled.div`
+    background-image: url(${db.bg});
+    flex: 1;
+    background-size: cover;
+    background-position: center;
+  `;
+
+  const QuizContainer = styled.div`
+    width: 100%;
+    max-width: 350px;
+    padding-top: 45px;
+    margin: auto 10%;
+    @media screen and (max-width: 500px) {
+      margin: auto;
+      padding: 15px;
+    }
+  `;
+
+ 
+
+export default function Home() {
+  return (
+      <QuizBackground BackgroundImage={db.bg}>
+        <Head>
+          <title>Alura</title>
+        </Head>
+        <QuizContainer>
+             <Widget>
+              <Widget.Header>
+                    <h1>The Legend of Zelda</h1>
+                 </Widget.Header>
+               <Widget.Content>
+                     <p>Lorem ....</p>
+               </Widget.Content>
+             </Widget>
+             <Widget>
+              <Widget.Content>
+                  <Widget.Header>
+                    <h1>The Legend of Zelda</h1>
+                 </Widget.Header>
+                  <p>Lorem ....</p>
+              </Widget.Content>
+            </Widget>
+            <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="www.google.com"/>
+      </QuizBackground>
+  );
+}
